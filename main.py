a139044e45999e23219a4d60b25c2492072b8e2f
FatAndreasbot <guva.andrej.01@gmail.com>
2023-10-16 05:42:29 +0700

    testing gtk

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
-import sys
-
 import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
 
 
-gi.require_version("Gtk", "4.0")
-from gi.repository import GLib, Gtk
+class AppWindow(Gtk.ApplicationWindow):
+
+    def __init__(self, app):
+
+        super(AppWindow, self).__init__(application=app)
+
+        self.init_ui()
+
+    def init_ui(self):
+
+        self.set_title('Check button')
+
+        box = Gtk.Box.new(Gtk.Orientation.VERTICAL, 0)
+        box.set_margin_start(5)
+        box.set_margin_top(5)
+
+        cbtn = Gtk.CheckButton.new_with_label('Show title')
+        cbtn.set_active(True)
+        cbtn.connect('toggled', self.on_toggle)
+
+        box.append(cbtn)
+
+        self.set_child(box)
+        self.set_default_size(450, 350)
+
+    def on_toggle(self, wid):
+
+        if wid.get_active():
+            self.set_title('CheckButton')
+        else:
+            self.set_title('')
 
 
-class MyApplication(Gtk.Application):
-    def __init__(self):
-        super().__init__(application_id="com.example.MyGtkApplication")
-        GLib.set_application_name('My Gtk Application')
+def on_activate(app):
 
-    def do_activate(self):
-        window = Gtk.ApplicationWindow(application=self, title="Hello World")
-        window.present()
+    win = AppWindow(app)
+    win.present()
 
 
-app = MyApplication()
-exit_status = app.run(sys.argv)
-sys.exit(exit_status)
+app = Gtk.Application(application_id='com.zetcode.CheckButton')
+app.connect('activate', on_activate)
+app.run(None)
