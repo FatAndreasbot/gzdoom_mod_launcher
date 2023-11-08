e45d4e02bee859c0df741c1f8f8b1a7ae3486096
FatAndreasbot <guva.andrej.01@gmail.com>
2023-11-08 16:55:06 +0700

    created models

---
+++ models/modfile.py
@@ -0,0 +1,9 @@
+
+# TODO
+# implement ID generation
+class Mod:
+    def __init__(self, path:str, name:str):
+        self.file_path = path
+        self.name = name
+        self.id = 0
+        
---
+++ models/modpack.py
@@ -0,0 +1,25 @@
+from models.modfile import Mod
+
+# # TODO
+# # implement ID generation
+# class Mod:
+#     def __init__(self, path:str, name:str):
+#         self.file_path = path
+#         self.name = name
+#         self.id = 0
+
+
+# TODO
+# implement ID generation
+class Modpack:
+    def __init__(self, name:str, description:str=""):
+        self.name = name
+        self.description = description
+        self.modlist:list[Mod] = []
+        self.id = 0
+        
+    def AddMod(self, mod:Mod):
+        self.modlist.append(mod)
+    
+
+        
