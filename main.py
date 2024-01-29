4c2c68ce77291aded2aa39e6bb3eb5481fac3b3d
FatAndreasbot <guva.andrej.01@gmail.com>
2024-01-30 02:43:59 +0700

    Check if that is OK

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
 import gi
 gi.require_version('Gtk', '4.0')
-gi.require_version('Adw', '1')
-from gi.repository import Gtk, Adw
+# gi.require_version('Adw', '1')
+from gi.repository import Gtk
+from gi.repository import Adw
 from views.mainmenu import MainMenu
 from models.dataMock import DataAcessMock
 from controllers.MainMenuController import MainMenuController
 
 
-
-
-
 if __name__ == "__main__":
-    
     app = Gtk.Application(application_id='DoomModManager')
-    
     appController = MainMenuController(DataAcessMock())
-    
-    
     
     def on_activate(app):
         win = MainMenu(app, appController)
         win.present()
-    
     
     app.connect('activate', on_activate)
     app.run(None)
